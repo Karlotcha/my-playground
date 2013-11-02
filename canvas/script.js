3ab16452725339b1c7cb1b76b6c34151c5779942
Karlotcha <KHOA@HOUSETRIP.COM>
2013-11-02 21:54:10 +0000

    blue attack

--- canvas/script.js
+++ canvas/script.js
@@ -19,13 +19,19 @@ function Point (x, y, r, g, b, a) {
 }
 
 Point.prototype.move  = function () {
-  this.x = this.x + rand (2) - rand (10)
-  this.y = this.y + rand (2) - rand (10)
+  this.c += 1
 
-  // this.r = rand(255)
-  // this.g = rand(255)
-  // this.b = rand(255)
-  // this.a = rand(255)
+  if (this.c == 10) {
+    this.c = 0
+    this.d = rand(3)
+  }
+
+  if (this.d==0)
+    this.x -= 1
+  else if (this.d==1)
+    this.x += 1
+
+  this.y +=1
 
   this.draw()
 }
@@ -66,7 +72,7 @@ var c   = document.getElementById("canvas")
   , image = ctx.getImageData(0, 0, width, height)
 
 setInterval(function() {
-  new Point(width/2, height/2, rand(255), rand(255), rand(255), rand(255))
+  new Point(width/2, 0, 0, 0, 255, rand(255))
   Point.list.forEach(function(thiz, i){thiz.move()})
   updateCanvas()
 }, 1)
